0e31559c0a84e52ec3f8d054253695e8d6e61aee
李平 <3328178766@qq.com>
2020-11-02 10:18:33 +0800

    项目初始化-引入vant组件库和rem适配

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,47 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+// module.exports = {
+//   root: true,
+//   env: {
+//     node: true
+//   },
+//   extends: [
+//     'plugin:vue/essential',
+//     '@vue/standard'
+//   ],
+//   parserOptions: {
+//     parser: 'babel-eslint'
+//   },
+//   rules: {
+//     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+//     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+//   }
+// }
+
+
+// package.json文件中的eslint代码
+// "scripts": {
+//   "serve": "vue-cli-service serve",
+//   "build": "vue-cli-service build",
+//   "lint": "vue-cli-service lint"
+
+// },
+
+// "gitHooks": {
+//   "pre-commit": "lint-staged"
+// },
+// "lint-staged": {
+//   "*.{js,jsx,vue}": [
+//     "vue-cli-service lint",
+//     "git add"
+//   ]
+// }
+
+// "@vue/eslint-config-standard": "^5.1.2",
+// "babel-eslint": "^10.1.0",
+// "eslint": "^6.7.2",
+// "eslint-plugin-import": "^2.20.2",
+// "eslint-plugin-node": "^11.1.0",
+// "eslint-plugin-promise": "^4.2.1",
+// "eslint-plugin-standard": "^4.0.0",
+// "eslint-plugin-vue": "^6.2.2",
+// "lint-staged": "^9.5.0",
+// "@vue/cli-plugin-eslint": "~4.5.0",
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
     <!-- 路由出口 -->
-    <router-view/>
+    <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<div class="box1"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.box1{
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
+</style>
